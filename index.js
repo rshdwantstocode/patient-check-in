27a07ea7dd37c03467f1ecb38e41ac5bec6b616a
rshdwantstocode <rushseed02@gmail.com>
2024-10-19 11:44:28 +0800

    added try catch to the get, styled the log in page, added little animation

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import pg from 'pg';
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
+  try {
     res.render("login");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-
 app.listen(port, () => {
-    console.log('Server now running at Port '+ port );
+  console.log("Server now running at Port " + port);
 });
