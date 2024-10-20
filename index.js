691ac2743d5221142e7f18331714480dc1c27d62
rshdwantstocode <rushseed02@gmail.com>
2024-10-20 12:10:26 +0800

    Added the dotenv for the database, created a home file after log in and added a post in index.js for logging in

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
+env.config();
+
+const data = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,// default PostgreSQL port
+});
+
+data.connect()
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,6 +25,21 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   try {
     res.render("login");
+    
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/login", async (req, res)=>{
+  const {username, password} = req.body;
+  try {
+    const result = await data.query("SELECT username, password FROM admins");
+    if (result.rows[0].username === username && result.rows[0].password === password) {
+        res.render("home");
+    } else {
+      res.render("login");
+    }
   } catch (error) {
     console.log(error);
   }
